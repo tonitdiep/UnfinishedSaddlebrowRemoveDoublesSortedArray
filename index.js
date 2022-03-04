b386dce52d0d33c835662c30801b03d4e1467213
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-03-04 02:00:05 +0000

    solution

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var removeDuplicates = function(nums) {
-}
+    let index = 1;
+    for(let i = 0; i < nums.length -1; i++) {
+        if(nums[i] != nums[i+1]){
+
+            nums[index++] = nums[i+1]
+       
+        }
+    }
+    return index;
+};
 
 /*
 -will need a return 
