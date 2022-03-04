e0318ef2662682dcd92b23754598b50f201210bb
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-03-04 01:59:31 +0000

    brainstorming notes

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+}
 
+/*
+-will need a return 
+-two pointer approch
 
-
+*/
 
 /*
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
